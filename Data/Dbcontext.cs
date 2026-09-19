60b9b2abc0a7574acdc9c69197cdc09b037c9fa6
Lhoyd Rener <Lhoyd Rener@DESKTOP-VKRMBR2>
2026-09-20 00:32:33 +0800

    12:32

--- Data/Dbcontext.cs
+++ Data/Dbcontext.cs
@@ -22,8 +22,6 @@ namespace CRUD.Data
         public DbSet<BorrowItem> BorrowItems { get; set; }
         public DbSet<UserModel> Users { get; set; }
         public DbSet<Penalty> Penalties { get; set; }
-        public DbSet<Reservation> Reservation { get; set; }
-        public DbSet<ReservationItems> ReservationItems{ get; set; }
 
         protected override void OnModelCreating(
            ModelBuilder modelBuilder)
